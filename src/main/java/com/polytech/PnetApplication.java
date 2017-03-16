5918068d3b2d218dde5345c8c48aa5d0c056724c
zeroual <abid1991@hotmail.fr>
2017-03-16 09:53:40 +0100

    first commit

---
+++ src/main/java/com/polytech/PnetApplication.java
@@ -0,0 +1,12 @@
+package com.polytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnetApplication.class, args);
+	}
+}
